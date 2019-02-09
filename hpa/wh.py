96bf875d66d6ccdad0075c85fc45956ccc662810
John Taylor <taylorcjohn@mac.com>
2019-02-09 14:02:29 +0000

    wh in git i hope

---
+++ hpa/wh.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# ...........................................................................
+# wh.py
+#
+# 2019-02-09 12:00
+#
+# ...........................................................................
+#
+# 2019-02-09 12:00 wh: who, why, where etc, - all questions
+#
+# ...........................................................................
+
+import math
+import argparse
+import sys
+import traceback
+import os
+
+
+# ...........................................................................
+def main(argv):
+    try:
+
+        print("\ngetcwd :{0}".format(os.getcwd()))
+        print('')
+        files_path = [os.path.abspath(x) for x in os.listdir()]
+        for x in files_path:
+            print("{0}".format(x))
+
+        # print("{0}".format(sys.version_info))
+        # print("default encoding {0}".format(sys.getdefaultencoding()))        # print("maximu# m Unicode code point {0}".format(hex(sys.maxunicode)))
+
+    except:
+        pass
+        traceback.print_exc()
+
+
+# ...........................................................................
+
+if __name__ == '__main__':
+    try:
+        main(sys.argv)
+    except:
+        traceback.print_exc()
+
+# ...........................................................................
